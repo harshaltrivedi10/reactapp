ef37647ad9a91e80db0ba5b0beae935b2e4f0fac
harshal <harshal30897@gmail.com>
2019-09-02 23:29:13 -0700

    09/02/2019 lecture 96

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,30 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.module.css';
 
-const cockpit = (props) => {
+const Cockpit = (props) => {
+  
+  useEffect (() => {
+    // can have multiple useEffects, to trigger it on different variables
+    console.log("[Cockpit.js] useEffect");
+    // HTTP request ......
+    setTimeout(() => {
+      alert("Saved data to cloud");
+    }, 1000);
+    return () => {
+      console.log("[Cockpit.js] cleanup in here");
+    }
+    // if empty array is passed, it runs just for the first time. As no variables are passed, it doesn't get triggered on any specific update
+  }, []);
 
-    let cssClasses = [];
-    let btnClass = '';
-    if (props.showPersons) {
-        btnClass = classes.Red;
+  useEffect (() => {
+    console.log('[Cockpit.js] 2nd useEffect');
+    return () => {
+      console.log("[Cockpit.js] cleanup in 2nd useEffect here");
     }
-    
-    if (props.persons.length <= 2) {
-      cssClasses.push( classes.red );
-    }
-    if (props.persons.length <= 1) {
-      cssClasses.push( classes.bold );
-    }
+  });
 
-    return(
-        <div className={classes.Cockpit}>
-            <h1>{props.title}</h1>
-            <p className={cssClasses.join(' ')}>This is really working!</p>
-            <button
-                className={btnClass}
-                onClick={ props.clicked }>Toggle Persons</button> 
-        </div>
-    );
+  let cssClasses = [];
+  let btnClass = '';
+  if (props.showPersons) {
+      btnClass = classes.Red;
+  }
+  
+  if (props.persons.length <= 2) {
+    cssClasses.push( classes.red );
+  }
+  if (props.persons.length <= 1) {
+    cssClasses.push( classes.bold );
+  }
+
+  return(
+      <div className={classes.Cockpit}>
+          <h1>{props.title}</h1>
+          <p className={cssClasses.join(' ')}>This is really working!</p>
+          <button
+              className={btnClass}
+              onClick={ props.clicked }>Toggle Persons</button> 
+      </div>
+  );
 }
 
-export default cockpit;
+export default Cockpit;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -21,6 +21,10 @@ class Persons extends Component {
         console.log(snapshot);
     }
 
+    componentWillUnmount () {
+        console.log('[Persons.js] componentDidUnmount');
+    }
+
     render () {
         console.log('[Persons.js] renderning...');
         return this.props.persons.map((person, index) => {
